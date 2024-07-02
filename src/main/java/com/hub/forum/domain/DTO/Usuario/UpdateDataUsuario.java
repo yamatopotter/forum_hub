c5e7adad2b652ea77019c42ee8a5be7b06bbd944
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-07-02 12:43:42 -0300

    add: Adição do JsonAlias para melhor representação dos dados

--- src/main/java/com/hub/forum/domain/DTO/Usuario/UpdateDataUsuario.java
+++ src/main/java/com/hub/forum/domain/DTO/Usuario/UpdateDataUsuario.java
@@ -1,12 +1,13 @@
 package com.hub.forum.domain.DTO.Usuario;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateDataUsuario(@NotNull Long id,
                                 String nome,
                                 String email,
                                 String senha,
-                                Long perfilId,
+                                @JsonAlias("perfil_id") Long perfilId,
                                 Boolean ativo) {
     public UpdateDataUsuario withEncryptedPassword(String password){
         return new UpdateDataUsuario(id, nome, email, password, perfilId, ativo);
